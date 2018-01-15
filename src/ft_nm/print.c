c970369e6f576f6340bf259bc994b54d2227072a
freccia <Freccia@users.noreply.github.com>
2018-01-15 20:05:42 +0100

    mismatches

--- src/ft_nm/print.c
+++ src/ft_nm/print.c
@@ -6,7 +6,7 @@
 /*   By: lfabbro <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/07 20:42:08 by lfabbro           #+#    #+#             */
-/*   Updated: 2018/01/15 18:50:59 by lfabbro          ###   ########.fr       */
+/*   Updated: 2018/01/15 19:57:30 by lfabbro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,15 @@ static char	get_symbol_type_sect(uint16_t n_sect, t_sections sects)
 	return ('S');
 }
 
-static char	get_symbol_type(uint8_t n_type, uint16_t n_sect, t_sections sects)
+static char	get_symbol_type(uint8_t n_type, uint16_t n_sect, uint16_t n_value,
+		t_sections sects)
 {
 	char	ret;
 
 	ret = '-';
-	if ((n_type & N_TYPE) == N_UNDF || (n_type & N_TYPE) == N_PBUD)
+	if ((n_type & N_TYPE) == N_UNDF)
+		ret = n_value != 0 ? 'C' : 'U';
+	if ((n_type & N_TYPE) == N_PBUD)
 		ret = 'U';
 	else if ((n_type & N_TYPE) == N_SECT)
 		ret = get_symbol_type_sect(n_sect, sects);
@@ -36,7 +39,8 @@ static char	get_symbol_type(uint8_t n_type, uint16_t n_sect, t_sections sects)
 		ret = 'A';
 	else if ((n_type & N_TYPE) == N_INDR)
 		ret = 'I';
-	if ((n_type & N_PEXT) || !(n_type & N_EXT))
+	//if ((n_type & N_PEXT) || !(n_type & N_EXT))
+	if (!(n_type & N_EXT))
 		ret = ft_tolower(ret);
 	return (ret);
 }
@@ -49,7 +53,7 @@ void		print_list_32(t_strtab *slist, t_sections sects)
 	ptr = slist;
 	while (ptr)
 	{
-		type = get_symbol_type(ptr->type, ptr->sect, sects);
+		type = get_symbol_type(ptr->type, ptr->sect, ptr->value, sects);
 		if (type != 'U' && type != 'u')
 			ft_printf("%08x %c %s\n", (uint32_t)ptr->value, type, ptr->strx);
 		else
@@ -66,7 +70,7 @@ void		print_list_64(t_strtab *slist, t_sections sects)
 	ptr = slist;
 	while (ptr)
 	{
-		type = get_symbol_type(ptr->type, ptr->sect, sects);
+		type = get_symbol_type(ptr->type, ptr->sect, ptr->value, sects);
 		if (type != 'U' && type != 'u')
 			ft_printf("%016lx %c %s\n", ptr->value, type, ptr->strx);
 		else
