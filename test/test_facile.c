de1d0f4bd21ea4993131f3d9ef11c82e56a549f6
freccia <Freccia@users.noreply.github.com>
2018-01-29 18:07:14 +0100

    gitignore+tests

---
+++ test/test_facile.c
@@ -0,0 +1,2 @@
+ #include <stdio.h>
+int main(void) { puts("Test facile"); return (0); }
---
+++ test/test_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+TMP1=/tmp/tmp1
+TMP2=/tmp/tmp2
+#DEBUG=#
+
+if [ $# -lt 1 ];then
+	echo "Usage: $0 /path/to/test/dir"
+	exit 1
+fi
+if [ ! -d $1 ];then
+	echo "Usage: $0 /path/to/test/dir"
+	exit 1
+fi
+
+for f in $1/*
+do
+	./ft_nm $f > $TMP1
+	nm $f > $TMP2
+	diff $TMP1 $TMP2  $DEBUG 1>&2 2>/dev/null
+	if [ $? -ne 0 ]; then
+		echo "File $f mismatch"
+#		exit 1
+	fi
+done
+
+
