003fb1b0223e7bfffc8bfe5c164f112c05f48818
freccia <Freccia@users.noreply.github.com>
2018-01-29 18:06:29 +0100

    norme

--- include/ft_common.h
+++ include/ft_common.h
@@ -6,7 +6,7 @@
 /*   By: lfabbro <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/12 18:51:54 by lfabbro           #+#    #+#             */
-/*   Updated: 2018/01/29 18:04:35 by lfabbro          ###   ########.fr       */
+/*   Updated: 2018/01/29 18:06:05 by lfabbro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@
 typedef struct s_archive	t_archive;
 typedef struct s_quad		t_quad;
 
-struct		s_quad
+struct				s_quad
 {
 	uint32_t		i;
 	uint32_t		tot;
